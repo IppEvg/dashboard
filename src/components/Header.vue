a0940b4adc29cdc61744e9933fa7b641209bf97c
IppEvg <ippevg@mail.ru>
2024-06-04 23:58:57 +0300

    reductedMin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,6 @@
 <script>
 import { ref } from 'vue'
 
-
 export default {
     name: 'HeaderComp',
     setup() {
@@ -16,16 +15,27 @@ export default {
 <template>
     <header>
         <div class="burgerMenu">
-            <v-btn class="burgerIcon" variant="text">
-                <img src="@/assets/menu50.png" alt="BM">
+            <div>
+                <v-btn class="burgerIcon" variant="text">
+                <img src="/assets/menu50.png" alt="BM" >
             </v-btn>
+            </div>
+            
         </div>
         <div class="linksMenu">
             <v-select solo v-for="link,id in links" :key="id"
             :items="items" density="comfortable"
             :label="link" variant="underlined"
             class="linksMenu_select remove-underline" v-field--none ></v-select>
-            <v-btn>Create</v-btn>
+            <v-btn class="linksMenu_btn" size="x-small">Create</v-btn>
+        </div>
+        <div class="loginMenu">
+            <v-text-field label="Search" append-inner-icon="mdi-magnify" variant="solo">
+            </v-text-field>
+            <v-select
+            :items="items" density="comfortable"
+            :label="link" variant="underlined"
+            class="linksMenu_select remove-underline" v-field--none ></v-select>
         </div>
     </header>
 </template>
@@ -33,18 +43,64 @@ export default {
 <style lang="scss">
 header{
     display: grid;
-    grid-template-columns: 1fr 4fr 5fr;
-    .burgerIcon{
-        display: flex;
+    grid-template-columns: 0.9fr 4fr 5fr;
+    background-color: rgba(32, 80, 129);
+    align-items:center;
+    color: white;
+    padding: 5px 0;
+    div{
+         display: flex;
         justify-content: left;
+        align-items: center;
+        gap: 5px;
     }
-    .linksMenu{
+    .burgerIcon{
+        img{
+            width: 30px;
+            height: 30px;
+        }
+    }
+    .linksMenu,.loginMenu{
         display: flex;
+        align-items: center;
         .linksMenu_select{
-            font-size: 12px;
+            font-size: 6px;
             min-width: 100px;
+            .v-field__outline::before, .v-field__outline::after {
+                width: 0;
+            }
+        }
+        &_btn{
+            background-color: rgba(59,129,197);
         }
     } 
+    .v-select .v-field.v-field{
+        font-size: 10px;
+    }
+    .v-input__details{
+        display: none;
+    }
+    .v-field__append-inner, .v-field__input, .v-field.v-field--variant-underlined .v-field__append-inner{
+        padding-top:0;
+    }
+    .v-label, .v-field__input{
+        font-size: 13px;
+    }
+    .loginMenu{
+        display: flex;
+        justify-content: end;
+        align-items: center;
+        
+        .v-field{
+        --v-field-input-padding-top:none;
+        background-color: rgba(65, 135, 214, 0.779);
+        }
+    }
+    .v-input{
+        max-width: 150px;
+    }
+
+    
 }
 </style>
 
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dashboard/'  : '/'
+    ? '/dashboard/'  : '/',
+    css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/assets/scss/vars.scss";`
+      }
+    }
+  }
 
 })
